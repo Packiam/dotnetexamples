1fb5826919605285587f644deb23552cf7f19107
Nahul <nahulguru2000@gmail.com>
2021-08-06 12:35:25 +0530

    Login Controller, Login View and Login Model Created

---
+++ MVC.Samples.Web/Areas/Guru/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Samples.Web.Areas.Guru.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Guru/LogIn
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC.Samples.Web/Areas/Guru/Models/LoginModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Samples.Web.Areas.Guru.Models
+{
+    public class LoginModel
+    {
+    }
+}
