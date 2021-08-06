2efe97c32f56212e20080225b481234f8beffb53
ravira03 <ravivarmara22@gmail.com>
2021-08-06 12:43:31 +0530

    Add login controller and home controller in area

---
+++ MVC.Samples.Web/Areas/Ravi/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Samples.Web.Areas.Ravi.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Ravi/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
