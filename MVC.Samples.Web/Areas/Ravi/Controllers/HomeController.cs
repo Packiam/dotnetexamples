f3da6b2c280ee895a2bb36055e271cf6805c3e46
ravira03 <ravivarmara22@gmail.com>
2021-08-06 13:09:33 +0530

    Removed Ravi controller and view

--- MVC.Samples.Web/Areas/Ravi/Controllers/HomeController.cs
+++ MVC.Samples.Web/Areas/Ravi/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC.Samples.Web.Areas.Ravi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,41 @@ namespace MVC.Samples.Web.Areas.Ravi.Controllers
     public class HomeController : Controller
     {
         // GET: Ravi/Home
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            RaviUserModel raviuserModel = new RaviUserModel();
+            raviuserModel.CityList = GetCityList();
+            raviuserModel.InterestedList = GetIntrestedList();
+            return View(raviuserModel);
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(RaviUserModel raviuser)
+        {
+            raviuser.CityList = GetCityList();
+            raviuser.InterestedList = GetIntrestedList();
+            //DB Store the data.
+            return View(raviuser);
+        }
+
+        private List<SelectListItem> GetCityList()
+        {
+            List<SelectListItem> cityList = new List<SelectListItem>();
+            cityList.Add(new SelectListItem() { Text = "Tirunelveli", Value = "TEN" });
+            cityList.Add(new SelectListItem() { Text = "Tuticorin", Value = "TUT" });
+            cityList.Add(new SelectListItem() { Text = "Theni", Value = "THE" });
+            return cityList;
+        }
+
+        private List<SelectListItem> GetIntrestedList()
+        {
+            List<SelectListItem> intrest = new List<SelectListItem>();
+            intrest.Add(new SelectListItem() { Text = "DotNet", Value = "DOT" });
+            intrest.Add(new SelectListItem() { Text = "React", Value = "REC" });
+            intrest.Add(new SelectListItem() { Text = "Angulur", Value = "ANG" });
+            return intrest;
         }
     }
 }
