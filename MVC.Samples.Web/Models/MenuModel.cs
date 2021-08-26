2382a970aa39a31082adf50afb423ca699501161
jebakumar86 <jebakumar86@gmail.com>
2021-08-26 16:11:30 +0530

    Just added menu rendering while login

---
+++ MVC.Samples.Web/Models/MenuModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Samples.Web.Models
+{
+    public class MenuModel
+    {
+        public string MenuName { get; set; }
+        public string ControllerName { get; set; }
+        public string  Action { get; set; }
+    }
+}
